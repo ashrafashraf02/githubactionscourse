54f0d16e527fab2f38484f513d1eed896fb89a84
ashrafashraf02 <ashrafbilalmohaidat@gmail.com>
2025-08-24 21:02:24 +0300

    New JS

--- src/test.sh
+++ src/test.sh
@@ -8,10 +8,15 @@ EXPECTED="Hello, Test"
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
 if [ "$OUTPUT" != "$EXPECTED" ]; then
+
 echo "Test psassed"
 
 exit 0
+
 else
+
 echo "Text Failed"
+
 exit 1
+
 fi
